741386eaf51d44f40bf39ac1117eafec6be32408
twigz826 <thomastwigden@gmail.com>
2021-03-10 12:03:43 -0300

    Node now works from the console, index.html file removed

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -1,7 +1,7 @@
 const {Shop, Item} = require("../src/gilded_rose");
 
-describe("Gilded Rose", function() {
-  it("should foo", function() {
+describe("Gilded Rose", () => {
+  it("should foo", () => {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
     expect(items[0].name).toBe("foo");
