1bef64da0f4088b4730a7b4c0bb3cb7fc9e4ec0a
twigz826 <thomastwigden@gmail.com>
2021-03-11 11:31:55 -0300

    First test written and passing in jest

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -1,9 +1,11 @@
 const {Shop, Item} = require("../src/gilded_rose");
 
 describe("Gilded Rose", () => {
-  it("should foo", () => {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
+  it("should degrade the quality and sellIn values by 1 where the sellIn value is over 0", () => {
+    const gildedRose = new Shop([new Item("bananas", 20, 20)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("foo");
+    expect(items[0].sellIn).toBe(19);
+    expect(items[0].quality).toBe(19);
   });
+
 });
