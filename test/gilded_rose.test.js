0c1965e6fa55ada05ed5de9272cf9a8cc4c1a24a
twigz826 <thomastwigden@gmail.com>
2021-03-11 11:54:53 -0300

    Added a describe block for normal items to make the the grouping of tests easier to understand

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -1,23 +1,28 @@
 const {Shop, Item} = require("../src/gilded_rose");
 
 describe("Gilded Rose", () => {
-  it("should degrade the quality and sellIn values by 1 where the sellIn value is over 0", () => {
-    const gildedRose = new Shop([new Item("bananas", 20, 20)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].sellIn).toBe(19);
-    expect(items[0].quality).toBe(19);
-  });
 
-  it("should degrade the quality by 2 where the sellIn value is negative", () => {
-    const gildedRose = new Shop([new Item("bananas", -1, 20)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].quality).toBe(18);
-  });
+  describe("normal items", () => {
 
-  it("quality can never go below 0", () => {
-    const gildedRose = new Shop([new Item("bananas", 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].quality).toBe(0);
+    it("should degrade the quality and sellIn values by 1 where the sellIn value is over 0", () => {
+      const gildedRose = new Shop([new Item("bananas", 20, 20)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(19);
+      expect(items[0].quality).toBe(19);
+    });
+
+    it("should degrade the quality by 2 where the sellIn value is negative", () => {
+      const gildedRose = new Shop([new Item("bananas", -1, 20)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(18);
+    });
+
+    it("quality can never go below 0", () => {
+      const gildedRose = new Shop([new Item("bananas", 0, 0)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(0);
+    });
+
   });
 
 });
