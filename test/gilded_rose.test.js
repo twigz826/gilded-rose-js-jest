ea8eeecff2a52c4acb5c0e728a04dc04b1d0e787
twigz826 <thomastwigden@gmail.com>
2021-03-11 11:34:52 -0300

    Second test written and passing to confirm quality decreases by 2 when sellIn value is negative

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -8,4 +8,10 @@ describe("Gilded Rose", () => {
     expect(items[0].quality).toBe(19);
   });
 
+  it("should degrade the quality by 2 where the sellIn value is negative", () => {
+    const gildedRose = new Shop([new Item("bananas", -1, 20)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toBe(-2);
+    expect(items[0].quality).toBe(18);
+  });
 });
